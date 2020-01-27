fc3246f5aefe378a6558564756747438d53c6d2e
edyson03 <edysonleal@wposs.com>
2020-01-27 16:27:21 -0500

    Se modifico la clase conexion y login

---
+++ app/src/main/java/com/example/duvan/wifix_v2/Clases/Conexion.java
@@ -0,0 +1,146 @@
+package com.example.duvan.wifix_v2.Clases;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.duvan.wifix_v2.LoginActivity;
+import com.example.duvan.wifix_v2.MainActivity;
+import com.example.duvan.wifix_v2.MainEmpleadoActivity;
+import com.example.duvan.wifix_v2.MasterMainActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Conexion {
+
+    public static final String SERVER_LOCAL = "192.168.137.1";
+    public static final String URL = "http://wifix.com.co/ServiciosWeb/";
+
+    private static RequestQueue queue;
+
+    public Conexion(){ }
+
+    public static void login(final Context contexto, final String cedula, final String contraseña) {
+
+        queue = Volley.newRequestQueue(contexto);
+
+        if (cedula.isEmpty() || contraseña.isEmpty()) {
+            Toast.makeText(contexto, "Por favor ingrese usuario y contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String urlEnvio = URL + "loginBD.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlEnvio,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        if(response.equals("0")) {
+                            Toast.makeText(contexto, "¡Usuario y/o contraseña incorrectos!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            try {
+                                JSONArray jsonArray = new JSONArray(response);
+                                String cedula = jsonArray.getJSONObject(0).getString("cedula");
+                                String tipo = jsonArray.getJSONObject(0).getString("id_tipoempleado");
+                                String tienda = jsonArray.getJSONObject(0).getString("tienda");
+                                // ABRIMOS UNA NUEVA ACTIVITY Y MANDAMOS LOS DATOS QUE SE VAN A MOSTRAR
+                                Toast.makeText(contexto, "--> " + tipo, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent();
+                                if(tipo.equals("1")){
+                                    intent = new Intent(contexto, MasterMainActivity.class);
+                                    intent.putExtra("cedula", cedula);
+                                    intent.putExtra("tienda", tienda);
+                                } else if(tipo.equals("2")){
+                                    intent = new Intent(contexto, MainActivity.class);
+                                    intent.putExtra("cedula", cedula);
+                                    intent.putExtra("tienda", tienda);
+                                } else if(tipo.equals("3")){
+                                    intent = new Intent(contexto, MainEmpleadoActivity.class);
+                                    intent.putExtra("cedula", cedula);
+                                    intent.putExtra("tienda", tienda);
+                                }
+                                contexto.startActivity(intent);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(contexto, "A ocurrido un error", Toast.LENGTH_SHORT).show();
+                        Log.e("TAG", error.toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("cedula", cedula);
+                parametros.put("password", contraseña);
+                return parametros;
+            }
+        };
+        queue.add(stringRequest);
+    }
+
+    public static void cargarProductos(final Context contexto){
+
+    }
+
+    public static void realizarVenta(final Context contexto, final String producto, final int precio, final int cantidad, final String cedula){
+
+        queue = Volley.newRequestQueue(contexto);
+
+        String urlEnvio = URL + "loginBD.php";
+        final String pre = String.valueOf(precio);
+        final String cant = String.valueOf(cantidad);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, urlEnvio,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        if(response.equals("0")) {
+                            Toast.makeText(contexto, "¡Usuario y/o contraseña incorrectos!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            try {
+                                JSONArray jsonArray = new JSONArray(response);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(contexto, "A ocurrido un error", Toast.LENGTH_SHORT).show();
+                        Log.e("TAG", error.toString());
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("cedula", cedula);
+                parametros.put("producto", producto);
+                parametros.put("cantidad", cant);
+                parametros.put("precio", pre);
+                return parametros;
+            }
+        };
+    }
+}
--- app/src/main/java/com/example/duvan/wifix_v2/LoginActivity.java
+++ app/src/main/java/com/example/duvan/wifix_v2/LoginActivity.java
@@ -7,18 +7,15 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.duvan.wifix_v2.Clases.Conexion;
 
 import org.json.JSONArray;
 
@@ -26,7 +23,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -39,11 +35,13 @@ public class LoginActivity extends Activity implements View.OnClickListener{
     private SharedPreferences preferences;
     private String tienda;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         cargarPreferencias();
+
         progressDialog = new ProgressDialog(LoginActivity.this);
         //preferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
         usuario = (EditText) findViewById(R.id.txtUser);
@@ -93,119 +91,22 @@ public class LoginActivity extends Activity implements View.OnClickListener{
         ConnectivityManager con = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = con.getActiveNetworkInfo();
         if(networkInfo != null && networkInfo.isConnected()){
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    final String resultado = enviarDatosGET(usuario.getText().toString(), contraseña.getText().toString());
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            int r = obtenerDatosJSON(resultado);
-                            int tipo = cargarDatos(resultado);
-                            if (usuario.getText().toString().isEmpty() || contraseña.getText().toString().isEmpty()) {
-                                Toast.makeText(getApplicationContext(), "¡Complete los campos!", Toast.LENGTH_LONG).show();
-                            } else {
-                                if (r > 0) {
-                                    progressDialog.dismiss();
-                                    if (tipo ==  1){
-                                        Toast.makeText(getApplicationContext(), "Has iniciado sesión como Gerente", Toast.LENGTH_LONG).show();
-                                        Intent intent = new Intent(getApplicationContext(), GerenteMainActivity.class);
-                                        intent.putExtra("cedula", usuario.getText().toString());
-                                        savePreferences(usuario.getText().toString(),contraseña.getText().toString(), tienda);
-                                        goToMain();
-                                        startActivity(intent);
-                                    } else if(tipo == 2){
-                                        Toast.makeText(getApplicationContext(), "Has iniciado sesión como Administrador", Toast.LENGTH_LONG).show();
-                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                        intent.putExtra("cedula", usuario.getText().toString());
-                                        savePreferences(usuario.getText().toString(),contraseña.getText().toString(), tienda);
-                                        goToMain();
-                                        startActivity(intent);
-                                    } else if(tipo == 3){
-                                        Toast.makeText(getApplicationContext(), "Has iniciado sesión como Empleado", Toast.LENGTH_LONG).show();
-                                        Intent intent = new Intent(getApplicationContext(), MainEmpleadoActivity.class);
-                                        intent.putExtra("cedula", usuario.getText().toString());
-                                        savePreferences(usuario.getText().toString(),contraseña.getText().toString(), tienda);
-                                        goToMain();
-                                        startActivity(intent);
-                                    }
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Usuario y/o contraseña incorrectos", Toast.LENGTH_SHORT).show();
-                                    //Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_SHORT).show();
-                                    progressDialog.hide();
-                                }
-                            }
-                            progressDialog.hide();
-                        }
-                    });
-                }
-            };
-            thread.start();
+            Conexion.login(this, usuario.getText().toString(), contraseña.getText().toString());
+            //Bundle datos = this.getIntent().getExtras();
+            //String cedula = datos.getString("cedula");
+            //savePreferences(cedula, cedula);
+            //goToMain();
+            progressDialog.dismiss();
         } else {
             Toast.makeText(getApplicationContext(), "Verifique su conexión a internet",Toast.LENGTH_SHORT).show();
             progressDialog.dismiss();
         }
     }
 
-    //METODO PARA ENVIAR LOS DATOS AL SERVIDOR LOCAL
-    public String enviarDatosGET(String usu, String pass){
-        URL url = null;
-        String linea = "";
-        int respuesta = 0;
-
-        StringBuilder resul = null;
-        String url_local = "http://192.168.56.1/ServiciosWeb/loginBD.php?";
-        String url_aws = "http://18.228.235.94/wifix/ServiciosWeb/loginBD.php?";
-
-        try{
-            //LA IP SE CAMBIA CON RESPECTO O EN BASE A LA MAQUINA EN LA CUAL SE ESTA EJECUTANDO
-            url = new URL(url_aws + "cedula=" + usu + "&password=" + pass);
-            HttpURLConnection conection = (HttpURLConnection) url.openConnection();
-            respuesta = conection.getResponseCode();
-            resul = new StringBuilder();
-            if (respuesta == HttpURLConnection.HTTP_OK){
-                InputStream inputStream = new BufferedInputStream(conection.getInputStream());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                while ((linea = reader.readLine()) != null){
-                    resul.append(linea);
-                }
-            }
-        }catch (Exception e){
-            return e.getMessage();
-        }
-        return resul.toString();
-    }
-
-    public int obtenerDatosJSON(String response){
-        int res = 0;
-        try {
-            JSONArray jsonArray = new JSONArray(response);
-            if (jsonArray.length() > 0 ){
-                res = 1;
-            }
-        }catch (Exception e){}
-        return res;
-    }
-
-    public int cargarDatos(String response) {
-        int tipo = 0;
-        try {
-            JSONArray jsonArray = new JSONArray(response);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                tipo = Integer.parseInt(jsonArray.getJSONObject(i).getString("id_tipoempleado"));
-                tienda = jsonArray.getJSONObject(i).getString("tienda");
-            }
-        } catch (Exception ex) {
-            Toast.makeText(getApplicationContext(), "Error: " + ex, Toast.LENGTH_SHORT).show();
-        }
-        return tipo;
-    }
-
-    private void savePreferences(String cedula, String pass, String tienda){
+    private void savePreferences(String cedula, String tienda){
         SharedPreferences preferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString("cedula", cedula);
-        editor.putString("pass", pass);
         editor.putString("tienda", tienda);
         editor.commit();
     }
